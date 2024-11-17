a581a344541cf8a808f00dadc08b351ba86471c8
Beenaa Salian <beenaasalian09@gmail.com>
2024-11-16 23:17:36 -0500

    historical table create statements

---
+++ azure_data_studio/historical_tables/CREATE_TABLES.sql
@@ -0,0 +1,47 @@
+CREATE TABLE AAPL_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
+
+CREATE TABLE NVDA_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
+
+CREATE TABLE SNOW_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
+
+CREATE TABLE UBER_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
+
+CREATE TABLE TSLA_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
+
+CREATE TABLE AMD_Historical (
+    timestamp DATE,
+    [open] FLOAT,
+    high FLOAT,
+    low FLOAT,
+    [close] FLOAT
+);
